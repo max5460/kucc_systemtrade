f01a551180fd32fffe18a9316f95472e21db6655
max5460 <lmc448.rvuevv@gmail.com>
2020-05-10 09:11:52 +0900

    機能追加第3弾
    ・株価CSV取得メソッドを差分更新用メソッド、洗い替え更新メソッドに分岐
    ・DMI、ADX計算関数実装
    ・株価CSV取得メソッドのリファクタリング
    ・機械学習分析メソッドのリファクタリング

--- main/stockPriceAnalize/StockPriceCSV.py
+++ main/stockPriceAnalize/StockPriceCSV.py
@@ -3,39 +3,80 @@ from bs4 import BeautifulSoup
 import re
 import pandas as pd
 import time
-from CommonConstant import CSVFILE_STORE_FOLDER_NAME
+from CommonConstant import CSVFILE_STORE_FOLDER_NAME, DROP_COLUMN_NAME_DATE, CSVFILE_EXTENSION
+import os
+from glob import glob
 
 SOURCE_URL = 'https://kabuoji3.com/stock/'
 CSV_NAME_UNUSABLE_WORDS = r'[\\/:*?"<>|]+'
-STOCK_NUMBER_PARAMETER = 'stock/[0-9]+'
+BRAND_CODE_PARAMETER = 'stock/[0-9]+'
 PAGE_PARAMETER = '\?page=[0-9]+'
-STOCK_NUMBER_AND_YEAR_PARAMETER = 'stock/[0-9]+/[0-9]+'
+BRAND_CODE_AND_YEAR_PARAMETER = 'stock/[0-9]+/[0-9]+'
+
+
+def __GetExistBrandDictionary():
+    existBrandDDict = {}
+    for csvFile in glob(CSVFILE_STORE_FOLDER_NAME + '//*' + CSVFILE_EXTENSION):
+        csvFileName = os.path.splitext(os.path.basename(csvFile))[0]
+
+        if csvFileName in existBrandDDict:
+            continue
+
+        existBrandDDict[csvFileName] = pd.read_csv(csvFile, encoding='UTF-8')
+
+    return existBrandDDict
+
+
+def __GetBrandCodeFromCSVFile(csvFile):
+    csvFileName = os.path.splitext(os.path.basename(csvFile))[0]
+    companyNameFromCSVFile = re.search(' .*', csvFileName)[0]
+    return csvFileName.replace(companyNameFromCSVFile, '')
 
 
 def __stripInvalidCharacterFromFileName(fileName):
     return re.sub(CSV_NAME_UNUSABLE_WORDS, '', fileName)
 
 
-def __GetStockPriceDataFrame(urlTag):
+def __DeleteDisappearedBrandCSV(existBrandDDict, brandOnWeb):
+    for brand in existBrandDDict.keys():
+        if brand not in brandOnWeb:
+            os.remove(CSVFILE_STORE_FOLDER_NAME + '//' + brand + CSVFILE_EXTENSION)
+
+
+def __GetExistBrandStockPriceDataFrame(urlTag, existBrandDDict, csvFileName):
+
+    latestStockPriceDataFrame = pd.read_html(urlTag.attrs['href'])[0]
+    time.sleep(1)
+    mergedStockPriceDataFrame = pd.concat([latestStockPriceDataFrame, existBrandDDict[csvFileName]])
+    mergedStockPriceDataFrame.drop_duplicates(inplace=True)
+
+    return mergedStockPriceDataFrame
+
+
+def __GetAllDayStockPriceDataFrame(urlTag):
     individualStockPriceDataFrame = pd.DataFrame(None)
 
     response_IndividualStockPricePerYear = request.urlopen(urlTag.attrs['href'])
     bs_GetIndividualStockPricePerYear = BeautifulSoup(response_IndividualStockPricePerYear, 'html.parser')
     time.sleep(1)
 
-    for individualStockPricePerYearTag in bs_GetIndividualStockPricePerYear.find_all("a", href=re.compile(STOCK_NUMBER_AND_YEAR_PARAMETER)):
+    for individualStockPricePerYearTag in bs_GetIndividualStockPricePerYear.find_all("a", href=re.compile(BRAND_CODE_AND_YEAR_PARAMETER)):
         individualStockPriceFromHtml = pd.read_html(individualStockPricePerYearTag.attrs['href'])
 
         if individualStockPriceFromHtml is None:
             continue
 
         individualStockPriceDataFrame = pd.concat([individualStockPriceDataFrame, individualStockPriceFromHtml[0]])
+        time.sleep(1)
 
-    individualStockPriceDataFrame = individualStockPriceDataFrame.sort_values('日付')
     return individualStockPriceDataFrame
 
 
 def ExportIndividualStockPrice():
+
+    existBrandDDict = __GetExistBrandDictionary()
+    brandOnWeb = []
+
     try:
         response_StockPriceTop = request.urlopen(SOURCE_URL)
         bs_GetStockPriceTop = BeautifulSoup(response_StockPriceTop, 'html.parser')
@@ -49,12 +90,23 @@ def ExportIndividualStockPrice():
             bs_GetIndividualStockPrice = BeautifulSoup(response_IndividualStockPrice, 'html.parser')
             time.sleep(1)
 
-            for urlTag in bs_GetIndividualStockPrice.find_all("a", href=re.compile(STOCK_NUMBER_PARAMETER)):
-                outIndividualStockPriceDataFrame = __GetStockPriceDataFrame(urlTag)
-                csvFilePath = CSVFILE_STORE_FOLDER_NAME + '\\' + __stripInvalidCharacterFromFileName(urlTag.text) + '.csv'
+            for urlTag in bs_GetIndividualStockPrice.find_all("a", href=re.compile(BRAND_CODE_PARAMETER)):
+                csvFileName = urlTag.text
+
+                if csvFileName in existBrandDDict:
+                    outIndividualStockPriceDataFrame = __GetExistBrandStockPriceDataFrame(urlTag, existBrandDDict, csvFileName)
+                else:
+                    outIndividualStockPriceDataFrame = __GetAllDayStockPriceDataFrame(urlTag)
+
+                csvFilePath = CSVFILE_STORE_FOLDER_NAME + '\\' + __stripInvalidCharacterFromFileName(csvFileName) + CSVFILE_EXTENSION
+                outIndividualStockPriceDataFrame.sort_values(DROP_COLUMN_NAME_DATE, inplace=True)
                 outIndividualStockPriceDataFrame.to_csv(csvFilePath, index=False)
+
+                brandOnWeb.append(csvFileName)
 
     except Exception as ex:
         return str(ex)
+
+    __DeleteDisappearedBrandCSV(existBrandDDict, brandOnWeb)
 
     return None
