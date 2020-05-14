84afce092a3c85c42eb4c5c2211eae2cf0775639
Yally-661 <yukiyada922@gmail.com>
2020-05-14 16:36:11 +0900

    Update ExecuteDb.py

--- main/stockPriceAnalize/ExecuteDb.py
+++ main/stockPriceAnalize/ExecuteDb.py
@@ -2,30 +2,34 @@ import pyodbc
 from datetime import datetime
 
 def __login():
-
-    instance = "systemtrade.cztijmkhdo19.us-east-2.rds.amazonaws.com,1433"
-    user = "admin"
-    pasword = "v7JYRrWx"
-    db = "systemTrade"
-    connection = "DRIVER={SQL Server};SERVER=" + instance + ";uid=" + user + ";pwd=" + pasword + ";DATABASE=" + db
-
+    try:
+       instance = "systemtrade.cztijmkhdo19.us-east-2.rds.amazonaws.com,1433"
+       user = "admin"
+       pasword = "v7JYRrWx"
+       db = "systemTrade"
+       connection = "DRIVER={SQL Server};SERVER=" + instance + ";uid=" + user + ";pwd=" + pasword + ";DATABASE=" + db
+       
+    except Exception as ex:
+       print(ex)
+       sys.exit(1)
+       
     return pyodbc.connect(connection)
 
 
 def __update_execute(con,df):
 
     cursor = con.cursor()
-    for index,item in df.iterrows():
+    for index in range(len(df)):
        try:
           select = """SELECT 1 
                       FROM   PREDICT_RESULT 
-                      WHERE  BRAND_CODE = '""" + str(df.at[index,'BRAND_CODE']) + """'"""
+                      WHERE  BRAND_CODE = (N'""" + df.at[index,'BRAND_CODE'] + """')"""
           cursor.execute(select)
           rows = cursor.fetchval()
           #print(rows)
 
           if rows != 1:
-             values = "(N'" + str(df.at[index,'BRAND_CODE']) + "'),(N'" + str(df.at[index,'BRAND_DESC']) + "'),(N'" + str(df.at[index,'PREDICTION']) + "')," + \
+             values = "(N'" + df.at[index,'BRAND_CODE'] + "'),(N'" + df.at[index,'BRAND_DESC'] + "'),(N'" + df.at[index,'PREDICTION'] + "')," + \
                       str(df.at[index,'ACCURACY']) + "," + str(df.at[index,'TRAINING_DATA_COUNT']) + ",'" + datetime.now().strftime('%Y-%m-%d %H:%M:%S') + "'"
 
              sql = """INSERT
@@ -39,21 +43,19 @@ def __update_execute(con,df):
                                          )
                       VALUES (""" + values + """)"""
              cursor.execute(sql)
-             con.commit()
 
           else:
-             values = "PREDICTION = (N'" + str(df.at[index,'PREDICTION']) + "'),ACCURACY = " + str(df.at[index,'ACCURACY']) + \
+             values = "PREDICTION = (N'" + df.at[index,'PREDICTION'] + "'),ACCURACY = " + str(df.at[index,'ACCURACY']) + \
                       ",TRAINING_DATA_COUNT = " + str(df.at[index,'TRAINING_DATA_COUNT']) + ",PREDICT_DATE = '" + datetime.now().strftime('%Y-%m-%d %H:%M:%S') + "'"
 
              sql = """UPDATE PREDICT_RESULT 
                       SET """ + values + """ 
                       WHERE BRAND_CODE = '""" + str(df.at[index,'BRAND_CODE']) + """'"""
              cursor.execute(sql)
-             con.commit()
-
+          con.commit()
+          
        except Exception as ex:
-           cursor.close()
-           print(ex)
+          print(ex)
 
     cursor.close()
     return "FINISH"
@@ -61,5 +63,6 @@ def __update_execute(con,df):
 def update_db(dataFrame):
     con = __login()
     res = __update_execute(con,dataFrame)
+    con.close()
     
     return res
