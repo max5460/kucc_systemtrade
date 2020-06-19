9e690d13c64a2020442c86c3378c94054b62335f
max5460 <lmc448.rvuevv@gmail.com>
2020-06-20 03:50:49 +0900

    不要バックアップ削除

--- main_20200615/Log/oldLogDelete.bat
+++
@@ -1,2 +0,0 @@
-CD %~dp0
-forfiles /p %~dp0 /d -30 /m "*.log" /c "cmd /c del @file"
--- main_20200615/stockPriceAnalize/test3.py
+++
@@ -1,6 +0,0 @@
-from keras.models import load_model
-
-
-predictModel = load_model('test.h5')
-
-a = predictModel.predict
